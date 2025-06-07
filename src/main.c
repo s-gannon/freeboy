aeef3891e6f803c95f84ce0fb334dba3f833a715
Spencer <spencer.gannon@valpo.edu>
2025-06-07 11:05:29 -0500

    Archival commit; will come back to this later

--- src/main.c
+++ src/main.c
@@ -2,11 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <curses.h>
+#include <locale.h>
 
-#define FREEBOY_SCREEN_IMPLEMENTATION
-#include "freeboy_screen.h"
+void ncurses_setup(void){
+	setlocale(LC_ALL, "");
+	
+	initscr();
+	cbreak();
+	noecho();
+
+	intrflush(stdscr, FALSE);
+	keypad(stdscr, TRUE);
+}
+
+void print_stats(void){
+	for(int i = 0; i < COLS; i++)
+		mvprintw(0, i, "-");
+	mvprintw(0, 4, "STATS");
+}
 
 int main(void){
-	printf("Hello, world!\n");
-	return 0;
+	ncurses_setup();
+	print_stats();
+
+	refresh();
+	getch();
+
+	endwin();
+	return EXIT_SUCCESS;
 }
